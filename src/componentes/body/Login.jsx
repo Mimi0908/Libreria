09c9cb65ef908a5b2b69b0fa9109637c4375ef92
mimi0908 <emilygarciamontes@gmail.com>
2024-04-16 10:55:29 -0500

    cambios de imagen y anexos a guardado

--- src/componentes/body/Login.jsx
+++ src/componentes/body/Login.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import './login.css';
 import Header from '../header/Header';
 import Footer from '../footer/Footer';
+import iconGoogle from '../../../public/iconGoogle.png'
+import FacebookIcon from '@mui/icons-material/Facebook';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
@@ -20,7 +23,37 @@ const Login = () => {
                             <label for="exampleInputPassword1" className="form-label">Contraseña</label>
                             <input type="password" className="form-control" id="exampleInputPassword1" />
                         </div>
-                        <button type="submit" className="btn btn-danger">Ingresar</button>
+                        <div className="boxInt">
+                            <div className="d-flex align-items-center gap-1">
+                                <input className="form-check-input" type="checkbox" />
+                                <label className="form-check-label" >
+                                    Remember me.
+                                </label>
+                            </div>
+                            <div className="forget">
+                                <p className="text-center text-muted mt-3"><a href='#' className="fw-bold text-danger">Forgot your password?</a></p>
+                            </div>
+                        </div>
+                        <div className="cajabtn">
+                            <button type="submit" className="btn btn-danger btnL">Login</button>
+                        </div>
+                        <p className="text-center text-muted mt-3 mb-0">No tienes una cuenta? <Link to='/Sign-Up'><a href="#!" className="fw-bold text-danger"><u>Registrate</u></a></Link></p>
+                        <div className='p-3'>
+                            <div className='border-bottom text-center' id='linea'>
+                                <span className='bg-white px-3'>or</span>
+                            </div>
+
+                        </div>
+                        <div className='caja-botones'>
+                            <div className="btn d-flex gap-2 justify-content-center border mt-3 shadow-sm btn-google">
+                                <img src={iconGoogle} className='icon' />
+                                <p className='fw-semibold text-secondary mb-0'>Continue with Google</p>
+                            </div>
+                            <div className="btn btn-primary gap-2 d-flex gap-0 justify-content-center border mt-3 shadow-sm btn-facebook">
+                                <FacebookIcon  className='icon'/>
+                                <p className='fw-semibold mb-0'>Continue with Facebook</p>
+                            </div>
+                        </div>
                     </form>
                 </div>
             </div>
--- src/componentes/body/Sign_up.jsx
+++ src/componentes/body/Sign_up.jsx
@@ -8,8 +8,8 @@ import { Link } from 'react-router-dom';
 import Colombia from './colombia'
 
 const SignUp = () => {
-    const [depIndex, setdepIndex]=useState(-1)
-    const [munIndex, setmunIndex]=useState(-1)
+    const [depIndex, setdepIndex] = useState(-1)
+    const [munIndex, setmunIndex] = useState(-1)
     const [identificacionError, setIdentificacionError] = useState(false)
     const [nomError, setNomError] = useState(false)
     const [apellidoError, setApellidoError] = useState(false)
@@ -59,15 +59,23 @@ const SignUp = () => {
         setPassComparacion(false)
         setPasswordErrorRepeat(false)
     }
-    function handleChangeDepartamento(e){
-        const index=e.target.value;
+    function handleChangeDepartamento(e) {
+        const index = e.target.value;
         setdepIndex(index)
-        console.log("opcion seleccionada: "+depIndex)
+        const departamentoSeleccionado = Colombia[index].departamento;
+        setValues(prevState => ({
+            ...prevState,
+            departamento: departamentoSeleccionado
+        }));
     }
-    function handleChangeMunicipio(e){
-        const index=e.target.value;
+    function handleChangeMunicipio(e) {
+        const index = e.target.value;
         setmunIndex(index)
-        console.log("opcion seleccionada: "+munIndex)
+        const municipioSeleccionado = Colombia[depIndex].ciudades[index];
+        setValues(prevState => ({
+            ...prevState,
+            municipio: municipioSeleccionado
+        }));
     }
     const [values, setValues] = useState({
         identificacion: "",
@@ -77,8 +85,8 @@ const SignUp = () => {
         direccion: "",
         telefono: "",
         fechaNacimiento: "",
-        departamento:"",
-        municipio:"",
+        departamento: "",
+        municipio: "",
         password: "",
         passRepeat: ""
     });
@@ -90,83 +98,81 @@ const SignUp = () => {
         }
         setValues(newValues)
     }
-    const handleSubmit = async(e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault()
         let validPassword = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-.]).{8,}$/
         let validEmail = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
 
         if (values.identificacion.length < 5 || values.identificacion.length > 10 || values.identificacion.length === 0) {
             setIdentificacionError(true)
-            return
-        } else if (values.nombres.length < 3 || values.nombres.length === 0) {
+
+        } if (values.nombres.length < 3 || values.nombres.length === 0) {
             setNomError(true)
-            return
-        } else if (values.apellidos.length < 3 || values.apellidos.length === 0) {
+
+        } if (values.apellidos.length < 3 || values.apellidos.length === 0) {
             setApellidoError(true)
-            return
-        } else if (values.email.length === 0) {
+
+        } if (values.email.length === 0) {
             setEmailVacio(true)
-            return
+
         } else if (!validEmail.test(values.email)) {
             setEmailError(true)
-            return
-        } else if (values.direccion.length < 15) {
+
+        } if (values.direccion.length < 15) {
             setDireccionError(true)
-            return
-        } else if (values.telefono.length < 10 || values.telefono.length > 10) {
+
+        } if (values.telefono.length < 10 || values.telefono.length > 10) {
             setTelefonoError(true)
-            return
-        } else if (values.fechaNacimiento === "") {
+
+        } if (values.fechaNacimiento === "") {
             setFechaNacimientoError(true)
-            return
-        } 
-        else if (departamento.values === "-1") {
+
+        } if (departamento.values === "-1") {
             setDepartamentoError(true)
-            return
-        }
-        else if (municipioError.values === "-1") {
+
+        } if (municipioError.values === "-1") {
             setMunicipioError(true)
-            return
-        }else if (!validPassword.test(values.password)) {
+
+        } if (!validPassword.test(values.password)) {
             setPasswordError(true)
-            return
-        } else if (values.passRepeat.length === 0) {
+
+        } if (values.passRepeat.length === 0) {
             setPasswordErrorRepeat(true)
-            return
+
         } else if (values.password !== values.passRepeat) {
             setPassComparacion(true)
-            return
+
         }
         await fetch('http://localhost:3001/Sign-Up', {
-        method: 'POST',
-        headers: { "Content-Type": "application/json", "Accept": 'application/json' },
-        body: JSON.stringify(values)
-    })
-        .then(response => {
-           // console.log(response.status)
-            if (response.status === 200) {
+            method: 'POST',
+            headers: { "Content-Type": "application/json", "Accept": 'application/json' },
+            body: JSON.stringify(values)
+        })
+            .then(response => {
+                // console.log(response.status)
+                if (response.status === 200) {
+                    Swal.fire({
+                        title: "Usuario creado con éxito",
+                        icon: "success"
+                    })
+                    form.current.reset()
+                    window.location.hash = '/login'
+                }
+                if (response.status === 400) {
+                    Swal.fire({
+                        title: "No fue posible crear el usuario porque ya existe el correo ingresado " + values.email,
+                        icon: "error"
+                    })
+                }
+            })
+            .catch((error) => {
                 Swal.fire({
-                    title: "Usuario creado con éxito",
-                    icon: "success"
-                })
-                form.current.reset()
-                window.location.hash = '/login'
-            }
-            if (response.status === 400) {
-                Swal.fire({
-                    title: "No fue posible crear el usuario porque ya existe el correo ingresado " + values.email,
+                    title: "No fue posible finalizar el proceso de registro por un error interno del servidor",
                     icon: "error"
                 })
-            }
-        })
-        .catch((error) => {
-            Swal.fire({
-                title: "No fue posible finalizar el proceso de registro por un error interno del servidor",
-                icon: "error"
             })
-        })
     }
-    
+
     return (
         <div>
             <Header />
@@ -214,7 +220,7 @@ const SignUp = () => {
                             <label htmlFor="departamento" className="form-label">Departamento</label>
                             <select defaultValue="0" className="form-select" aria-label="Default select example" name='departamento' id='departamento' onChange={handleChangeDepartamento} onClick={depError}>
                                 <option value="0">Abrir el menú</option>
-                                {Colombia.map((item,e)=>(
+                                {Colombia.map((item, e) => (
                                     <option key={e} value={e}>{item.departamento}</option>
                                 ))}
                             </select>
@@ -224,7 +230,7 @@ const SignUp = () => {
                             <label htmlFor="municipio" className="form-label">Municipio</label>
                             <select defaultValue="" className="form-select" aria-label="Default select example" name='municipio' id='municipio' onChange={handleChangeMunicipio} onClick={munError}>
                                 <option value="0" >Abrir el menú</option>
-                                { depIndex > -1 && Colombia[depIndex].ciudades.map((item,e)=>(
+                                {depIndex > -1 && Colombia[depIndex].ciudades.map((item, e) => (
                                     <option key={e} value={e}>{item}</option>
                                 ))}
                             </select>
