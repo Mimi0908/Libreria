d99c772f3ea33bd7dca8552ac25fdd7744dcaf6c
mimi0908 <emilygarciamontes@gmail.com>
2024-07-02 18:02:07 -0500

    cambio de host

--- server/configDB/configDB.js
+++ server/configDB/configDB.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql2')
 const connection = mysql.createConnection({
     host: "sql10.freesqldatabase.com",
-    user: "sql10715859",
-    password: "hc2Nvrq521",
-    database: "sql10715859",
+    user: "sql10717599",
+    password: "PPLhmubHrc",
+    database: "sql10717599",
     port: 3306,
 
     // host: "sql10.freesqldatabase.com",
